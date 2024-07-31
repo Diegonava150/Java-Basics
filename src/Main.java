de60f24f28c1aa39b2172d42c9dcc2adb8d38bc8
Diego Navarro Ruiz <diegojose150@gmail.com>
2024-07-31 17:24:08 -0500

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter account owner name: ");
+        String ownerName = scanner.nextLine();
+        BankAccount account = new BankAccount(ownerName);
+
+        BankAccount.manageAccount(account, scanner);
+    }
+}
